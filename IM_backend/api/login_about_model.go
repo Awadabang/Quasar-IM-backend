420d6a4b5b311fab10fdc2da5b2b90436d3ca0e9
Awadabang <1765256254@qq.com>
2022-03-15 01:49:24 +0800

    change name

---
+++ IM_backend/api/login_about_model.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"time"
+
+	db "github.com/Awadabang/Quasar-IM/db/sqlc"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Request_register struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+//Login
+type userResponse struct {
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func newUserResponse(user db.User) userResponse {
+	return userResponse{
+		Username:  user.Username,
+		CreatedAt: user.CreatedAt,
+	}
+}
+
+type Response_login struct {
+	AccessToken string       `json:"access_token"`
+	User        userResponse `json:"user"`
+}
+
+type Request_login struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
