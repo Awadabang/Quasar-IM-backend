ea8f1bcc37049a211d4b6c3779c34f114fe1fde0
Awadabang <1765256254@qq.com>
2022-03-20 01:04:35 +0800

    route

--- IM_backend/conf/conf.go
+++ IM_backend/conf/conf.go
@@ -1,7 +1,7 @@
 /*
  * @Author: Awadabang
  * @Date: 2022-02-28 22:33:31
- * @LastEditTime: 2022-03-19 20:58:20
+ * @LastEditTime: 2022-03-20 01:04:27
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Quasar-IM-backend\IM_backend\conf\conf.go
@@ -32,7 +32,7 @@ var (
 )
 
 func Init() {
-	if err := LoadLocales("conf/locales/zh-cn.yaml"); err != nil {
+	if err := LoadLocales("./locales/zh-cn.yaml"); err != nil {
 		logging.Info(err) //日志内容
 		panic(err)
 	}
