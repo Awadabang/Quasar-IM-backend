2d2838d616039dc2e6a3f9c4bc523078cb7ab1de
Awadabang <1765256254@qq.com>
2022-03-21 02:01:21 +0800

    fix

--- IM_backend/service/find.go
+++ IM_backend/service/find.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Awadabang/Quasar-IM/conf"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -18,6 +19,7 @@ type SendSortMsg struct {
 	CreateAt int64  `json:"create_at"`
 }
 
+//向Mongodb中写入数据
 func InsertMsg(database string, id string, content string, read uint, expire int64) (err error) {
 	collection := conf.MongoDBClient.Database(database).Collection(id)
 	comment := Trainer{
@@ -30,20 +32,25 @@ func InsertMsg(database string, id string, content string, read uint, expire int
 	return
 }
 
-func FindMany(database string, sendId string, id string, time int64, pageSize int) (results []Result, err error) {
+func FindMany(database string, sendId string, id string, time int64, pageSize int) ([]Result, error) {
 	var resultsMe []Trainer
 	var resultsYou []Trainer
 	sendIdCollection := conf.MongoDBClient.Database(database).Collection(sendId)
 	idCollection := conf.MongoDBClient.Database(database).Collection(id)
 	// 如果不知道该使用什么context，可以通过context.TODO() 产生context
-	sendIdTimeCursor, err := sendIdCollection.Find(context.TODO(),
-		options.Find().SetSort(bson.D{{"startTime", -1}}), options.Find().SetLimit(int64(pageSize)))
-	idTimeCursor, err := idCollection.Find(context.TODO(),
-		options.Find().SetSort(bson.D{{"startTime", -1}}), options.Find().SetLimit(int64(pageSize)))
+	sendIdTimeCursor, err := sendIdCollection.Find(context.TODO(), options.Find().SetSort(bson.D{{Key: "startTime", Value: -1}}), options.Find().SetLimit(int64(pageSize)))
+	if err != nil {
+		return nil, err
+	}
+	var idTimeCursor *mongo.Cursor
+	idTimeCursor, err = idCollection.Find(context.TODO(), options.Find().SetSort(bson.D{{Key: "startTime", Value: -1}}), options.Find().SetLimit(int64(pageSize)))
+	if err != nil {
+		return nil, err
+	}
 	err = sendIdTimeCursor.All(context.TODO(), &resultsYou) // sendId 对面发过来的
 	err = idTimeCursor.All(context.TODO(), &resultsMe)      // Id 发给对面的
-	results, _ = AppendAndSort(resultsMe, resultsYou)
-	return
+	results, _ := AppendAndSort(resultsMe, resultsYou)
+	return results, nil
 }
 
 func FirsFindtMsg(database string, sendId string, id string) (results []Result, err error) {
@@ -55,8 +62,7 @@ func FirsFindtMsg(database string, sendId string, id string) (results []Result, 
 	filter := bson.M{"read": bson.M{
 		"&all": []uint{0},
 	}}
-	sendIdCursor, err := sendIdCollection.Find(context.TODO(), filter, options.Find().SetSort(bson.D{{
-		"startTime", 1}}), options.Find().SetLimit(1))
+	sendIdCursor, err := sendIdCollection.Find(context.TODO(), filter, options.Find().SetSort(bson.D{{Key: "startTime", Value: 1}}), options.Find().SetLimit(1))
 	if sendIdCursor == nil {
 		return
 	}
@@ -66,25 +72,16 @@ func FirsFindtMsg(database string, sendId string, id string) (results []Result, 
 		log.Println("sendIdCursor err", err)
 	}
 	if len(unReads) > 0 {
-		timeFilter := bson.M{
-			"startTime": bson.M{
-				"$gte": unReads[0].StartTime,
-			},
-		}
+		timeFilter := bson.M{"startTime": bson.M{"$gte": unReads[0].StartTime}}
 		sendIdTimeCursor, _ := sendIdCollection.Find(context.TODO(), timeFilter)
 		idTimeCursor, _ := idCollection.Find(context.TODO(), timeFilter)
-		err = sendIdTimeCursor.All(context.TODO(), &resultsYou)
+		_ = sendIdTimeCursor.All(context.TODO(), &resultsYou)
 		err = idTimeCursor.All(context.TODO(), &resultsMe)
 		results, err = AppendAndSort(resultsMe, resultsYou)
 	} else {
 		results, err = FindMany(database, sendId, id, 9999999999, 10)
 	}
-	overTimeFilter := bson.D{
-		{"$and", bson.A{
-			bson.D{{"endTime", bson.M{"&lt": time.Now().Unix()}}},
-			bson.D{{"read", bson.M{"$eq": 1}}},
-		}},
-	}
+	overTimeFilter := bson.D{{"$and", bson.A{bson.D{{Key: "endTime", Value: bson.M{"&lt": time.Now().Unix()}}}, bson.D{{Key: "read", Value: bson.M{"$eq": 1}}}}}}
 	_, _ = sendIdCollection.DeleteMany(context.TODO(), overTimeFilter)
 	_, _ = idCollection.DeleteMany(context.TODO(), overTimeFilter)
 	// 将所有的维度设置为已读
