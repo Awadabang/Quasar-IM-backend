22b96d1f436e7fecd047303734563f002bb8354d
Awadabang <1765256254@qq.com>
2022-03-20 16:15:15 +0800

    fix a little

--- IM_backend/api/friend_about_model.go
+++ IM_backend/api/friend_about_model.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-20 15:40:36
- * @LastEditTime: 2022-03-20 16:05:25
+ * @LastEditTime: 2022-03-20 16:15:04
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Quasar-IM-backend\IM_backend\api\friend_about_model.go
@@ -9,7 +9,7 @@
 package api
 
 type addfriendRequest struct {
-	Friendid int64
+	Friendid int64 `json:"friendid"`
 }
 
 type getfriendsRequest struct {
