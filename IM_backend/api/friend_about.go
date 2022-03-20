de1313992588b483b3107fc00e1eb7be6fe3675b
Awadabang <1765256254@qq.com>
2022-03-20 16:57:48 +0800

    不能添加自己为朋友

--- IM_backend/api/friend_about.go
+++ IM_backend/api/friend_about.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-20 15:40:21
- * @LastEditTime: 2022-03-20 16:50:59
+ * @LastEditTime: 2022-03-20 16:57:26
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \Quasar-IM-backend\IM_backend\api\friend_about.go
@@ -24,6 +24,10 @@ func (server *Server) Add_friend(ctx *gin.Context) {
 		return
 	}
 	authPayload := ctx.MustGet(middleware.AuthorizationPayloadKey).(*token.Payload)
+	if authPayload.Userid == req.Friendid {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "You can't add yourself be your friend"})
+		return
+	}
 	arg := db.AddFriendParams{
 		Owner:    authPayload.Userid,
 		FriendID: req.Friendid,
